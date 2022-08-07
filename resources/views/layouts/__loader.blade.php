b7fed8c6b68cca4a7088700252ea2dacf43826c3
Eduardo Haddad <eduardohhaddad@gmail.com>
2022-08-07 17:09:54 -0300

    AJuste para loader funcionar.

---
+++ resources/views/layouts/__loader.blade.php
@@ -0,0 +1,49 @@
+
+<!-- Inicio Loader padrão da página -->
+<div id="loader" class="loader">
+    <div class="plane-container">
+        {{--<img src="{{ asset('assets/img/icon/loader-infinity.svg') }}" />--}}
+        <div class="preloader-wrapper small active">
+            <div class="spinner-layer spinner-blue">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div><div class="gap-patch">
+                <div class="circle"></div>
+            </div><div class="circle-clipper right">
+                <div class="circle"></div>
+            </div>
+            </div>
+
+            <div class="spinner-layer spinner-red">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div><div class="gap-patch">
+                <div class="circle"></div>
+            </div><div class="circle-clipper right">
+                <div class="circle"></div>
+            </div>
+            </div>
+
+            <div class="spinner-layer spinner-yellow">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div><div class="gap-patch">
+                <div class="circle"></div>
+            </div><div class="circle-clipper right">
+                <div class="circle"></div>
+            </div>
+            </div>
+
+            <div class="spinner-layer spinner-green">
+                <div class="circle-clipper left">
+                    <div class="circle"></div>
+                </div><div class="gap-patch">
+                <div class="circle"></div>
+            </div><div class="circle-clipper right">
+                <div class="circle"></div>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Fim Loader padrão da página -->

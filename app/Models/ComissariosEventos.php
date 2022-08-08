41b4cd8f151e7829026ce9140a8674bfd9989529
Eduardo Haddad <eduardohhaddad@gmail.com>
2022-08-08 09:12:52 -0300

    Criação de models para tabelas do banco de dados.

---
+++ app/Models/ComissariosEventos.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ComissariosEventos extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/ComissariosIngressos.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ComissariosIngressos extends Model
+{
+    protected $primaryKey = 'id_comissario_ingresso';
+    protected $table = 'comissarios_ingressos';
+}
